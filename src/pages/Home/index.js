b27d9196ade445cccbdab85d13f4b4fed7959716
nnox <noxormy@gmail.com>
2023-01-04 05:55:45 +0400

    add link to articles

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,18 @@
 import React from "react"
 import "./index.sass"
+import {Link} from "react-router-dom"
+import {Card} from "antd"
+import Meta from "antd/es/card/Meta"
 
 
 function Home() {
     return (
-        <div>
+        <div className="home">
+            <Link to="/articles">
+                <Card cover={<img src="https://pix4free.org/assets/library/2021-01-21/originals/cohabitation_laws.jpg" alt="cover"/>}>
+                    <Meta title="Law articles" description="How to face with the law" />
+                </Card>
+            </Link>
         </div>
     )
 }
